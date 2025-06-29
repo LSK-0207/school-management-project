fbb009faeb56ced533b62b0abad443c992c3e4c8
LS <itsls2005@gmail.com>
2025-06-30 00:05:19 +0530

    re-installed prev v of NextJS and major changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,14 +28,14 @@ const Navbar = () => {
           <span className="text-xs leading-3 font-medium">John Doe</span>
           <span className="text-[10px] text-gray-500 text-right">Admin</span>
         </div>
-        <Image
+        {/* <Image
           src="/avatar.png"
           alt=""
           width={36}
           height={36}
           className="rounded-full"
-        />
-        {/* <UserButton /> */}
+        /> */}
+        <UserButton />
       </div>
     </div>
   );
