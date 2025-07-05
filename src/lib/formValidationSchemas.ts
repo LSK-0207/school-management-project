048f32593b11563cee99eabb0f1890a89888b2da
LS <itsls2005@gmail.com>
2025-07-06 00:19:54 +0530

    Project Finalised

---
+++ src/lib/formValidationSchemas.ts
@@ -0,0 +1,83 @@
+import { z } from "zod";
+
+export const subjectSchema = z.object({
+  id: z.coerce.number().optional(),
+  name: z.string().min(1, { message: "Subject name is required!" }),
+  teachers: z.array(z.string()), //arrays for storing teacher ids
+});
+
+export type SubjectSchema = z.infer<typeof subjectSchema>;
+
+export const classSchema = z.object({
+  id: z.coerce.number().optional(),
+  name: z.string().min(1, { message: "Subject name is required!" }),
+  capacity: z.coerce.number().min(1, { message: "Capacity is required" }),
+  gradeId: z.coerce.number().min(1, { message: "Grade is required" }),
+  supervisorId: z.coerce.string().optional(),
+});
+
+export type ClassSchema = z.infer<typeof classSchema>;
+
+//Teacher Schema
+
+export const teacherSchema = z.object({
+  id: z.string().optional(),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" })
+    .optional()
+    .or(z.literal("")),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address!" })
+    .optional()
+    .or(z.literal("")),
+  phone: z.string().optional(),
+  address: z.string(),
+  img: z.string().optional(),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.coerce.date({ message: "Birthday is required!" }),
+  sex: z.enum(["MALE", "FEMALE"], { message: "Sex is required!" }),
+  subjects: z.array(z.string()).optional(),
+});
+
+export type TeacherSchema = z.infer<typeof teacherSchema>;
+
+// Student Schema
+
+export const studentSchema = z.object({
+  id: z.string().optional(),
+  username: z
+    .string()
+    .min(3, { message: "Username must be at least 3 characters long!" })
+    .max(20, { message: "Username must be at most 20 characters long!" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long!" })
+    .optional()
+    .or(z.literal("")),
+  firstName: z.string().min(1, { message: "First name is required!" }),
+  lastName: z.string().min(1, { message: "Last name is required!" }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address!" })
+    .optional()
+    .or(z.literal("")),
+  phone: z.string().optional(),
+  address: z.string(),
+  img: z.string().optional(),
+  bloodType: z.string().min(1, { message: "Blood Type is required!" }),
+  birthday: z.coerce.date({ message: "Birthday is required!" }),
+  sex: z.enum(["MALE", "FEMALE"], { message: "Sex is required!" }),
+  gradeId: z.coerce.number().min(1, { message: "Grade is required" }),
+  classId: z.coerce.number().min(1, { message: "Class is required" }),
+  parentId: z.string().min(1, { message: "Parent Id is required" }),
+});
+
+export type StudentSchema = z.infer<typeof studentSchema>;
